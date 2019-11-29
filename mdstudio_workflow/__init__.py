a829f30c10039c4cc7ed062e37ee31332b545036
Marc van Dijk <marcvdijk@gmail.com>
2019-11-29 10:49:05 +0100

    Add dynamic package root path as __rootpath__

--- mdstudio_workflow/__init__.py
+++ mdstudio_workflow/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 __module__ = 'mdstudio_workflow'
 __docformat__ = 'restructuredtext'
 __version__ = '{major:d}.{minor:d}'.format(major=1, minor=0)
@@ -9,6 +11,7 @@ __date__ = '15 april 2016'
 __licence__ = 'Apache Software License 2.0'
 __url__ = 'https://github.com/MD-Studio/MDStudio_structures'
 __copyright__ = "Copyright (c) VU University, Amsterdam"
+__rootpath__ = os.path.dirname(__file__)
 __all__ = ['Workflow', '__version__']
 
 from .workflow_runner import WorkflowRunner as Workflow
